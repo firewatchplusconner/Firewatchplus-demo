f161c81f1ad20c4f45b878c7adea6ab1ed3f25ee
owencshoop <owencshoop@gmail.com>
2023-01-26 09:01:26 -0800

    update signup and login forms and signup route

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,94 +1,127 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+    const [errors, setErrors] = useState([]);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const user = useSelector((state) => state.session.user);
+    const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
+    const onSignUp = async (e) => {
+        e.preventDefault();
+        if (password === repeatPassword) {
+            const data = await dispatch(signUp(username, email, password, firstName, lastName));
+            if (data) {
+                setErrors(data);
+            }
+        }
+    };
+
+    const updateUsername = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const updateEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const updatePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const updateRepeatPassword = (e) => {
+        setRepeatPassword(e.target.value);
+    };
+
+    if (user) {
+        return <Redirect to="/" />;
     }
-  };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
-  if (user) {
-    return <Redirect to='/' />;
-  }
-
-  return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
-  );
+    return (
+        <div className="pad30lr fdcol w30vw ofhidden h100p aicen">
+            <div className="marlrauto mar20b">Sign Up</div>
+            <form onSubmit={onSignUp} className="w70p">
+                <div>
+                    {errors.map((error, ind) => (
+                        <div key={ind}>{error}</div>
+                    ))}
+                </div>
+                <div className="fdcol mar20b">
+                    <label>User Name</label>
+                    <input
+                        type="text"
+                        name="username"
+                        onChange={updateUsername}
+                        value={username}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="fdcol mar20b">
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        name="email"
+                        onChange={updateEmail}
+                        value={email}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="fdcol mar20b">
+                    <label>First Name</label>
+                    <input
+                        type="text"
+                        name="email"
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="fdcol mar20b">
+                    <label>Last Name</label>
+                    <input
+                        type="text"
+                        name="email"
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="fdcol mar20b">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={updatePassword}
+                        value={password}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="fdcol mar20b">
+                    <label>Repeat Password</label>
+                    <input
+                        type="password"
+                        name="repeat_password"
+                        onChange={updateRepeatPassword}
+                        value={repeatPassword}
+                        required={true}
+                        className="iflight bnone h40px"
+                    ></input>
+                </div>
+                <div className="jccen mar30t">
+                    <button type="submit" className="w100p h50px btndark pad0">
+                        Sign Up
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default SignUpForm;
