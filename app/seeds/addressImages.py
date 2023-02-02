ad127dea97575f4f1a35fa003cae4751cd2785f7
owencshoop <owencshoop@gmail.com>
2023-02-01 20:49:40 -0800

    add seed data for white house images

--- app/seeds/addressImages.py
+++ app/seeds/addressImages.py
@@ -1,7 +1,25 @@
-from app.models import db, environment, SCHEMA, AddressImage, Address
+from app.models import db, environment, SCHEMA, AddressImage, Address, User
 
 def seed_address_images():
-    pass
+    demo = User.query.get(1)
+
+    sanFran = Address.query.get(1)
+    newYork = Address.query.get(2)
+    whiteHouse = Address.query.get(3)
+
+    whiteHouseImg1 = AddressImage(address=whiteHouse, user=demo, title='Basement/Sub-basement Floor Plan', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/basement-subbasement-floor-plan.png')
+    whiteHouseImg2 = AddressImage(address=whiteHouse, user=demo, title='Ground Floor Axonometric', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/ground-floor-axonometric.jpg')
+    whiteHouseImg3 = AddressImage(address=whiteHouse, user=demo, title='Ground Floor Diagram', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/ground-floor-diagram.jpg')
+    whiteHouseImg4 = AddressImage(address=whiteHouse, user=demo, title='Ground Floor Plan', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/ground-floor-plan.jpg')
+    whiteHouseImg5 = AddressImage(address=whiteHouse, user=demo, title='First Floor Axonometric', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/first-floor-axonometric.jpg')
+    whiteHouseImg6 = AddressImage(address=whiteHouse, user=demo, title='First Floor Diagram', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/first-floor-diagram.jpg')
+    whiteHouseImg7 = AddressImage(address=whiteHouse, user=demo, title='First Floor Plan', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/first-floor-plan.jpg')
+    whiteHouseImg8 = AddressImage(address=whiteHouse, user=demo, title='Second Floor Diagram', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/second-floor-diagram.jpg')
+    whiteHouseImg9 = AddressImage(address=whiteHouse, user=demo, title='Second Floor Plan', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/second-floor-plan.jpg')
+    whiteHouseImg10 = AddressImage(address=whiteHouse, user=demo, title='Third Floor Diagram', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/third-floor-diagram.jpg')
+    whiteHouseImg11 = AddressImage(address=whiteHouse, user=demo, title='Third Floor Plan', url='https://ready-response-bucket.s3.us-west-1.amazonaws.com/third-floor-plan.jpg')
+    db.session.add(whiteHouse)
+    db.session.commit()
 
 def undo_address_images():
     if environment == "production":
