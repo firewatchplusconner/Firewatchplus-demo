598ae365bbfbb1d3d8dfacee344866431963a51b
connershoop <connershoop@gmail.com>
2023-05-31 11:55:09 -0400

    move to firewatchPlus brand

--- react-app/src/components/SplashPage/splashPage.js
+++ react-app/src/components/SplashPage/splashPage.js
@@ -2,24 +2,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./splashPage.css";
+import { Plus } from 'tabler-icons-react';
 
 const SplashPage = () => {
     const user = useSelector((state) => state.session.user);
 
     return (
         <div className="splash-page-container">
-            <div className="splash-page-header">Ready Response</div>
+            <div className="splash-page-header">Firewatch 
+            <Plus size={48} color="#ffe5b4" className="mar10r" />
+            </div>
             <div className="description-container">
                 <div className="description">
-                    Ready Response is designed to help responders before they
-                    arrive to an emergency. When dispatched to an address,
+                    Firewatch Plus is designed to help responders before and during a fire
+                    inspection. When dispatched to an address,
                     responders are able to access owner information, google maps
                     data, details from past inspections, and images/plans for the address. Sign up for
-                    Ready Response to be able manage addresses and inspections.
+                    Firewatch Plus to be able manage addresses and inspections.
                     Currently, all users are given add, update, and delete
                     permissions. In a scalable version, these permissions would
                     be limited to organization's admin or inspectors. Join our
-                    Ready Response community and be ready to respond!
+                    Firewatch Plus community and be ready to respond!
                 </div>
             </div>
                 {!user && (
